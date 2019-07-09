95f180fae1ec51f1b5c7b0b0084b207cb537e052
Dave <42207967+davelivingstone@users.noreply.github.com>
2019-07-09 17:14:34 +0300

    Calendar for september 2018
    Just trying to create a nicely formatted calendar. Of course, you could just import calendar and use print(calendar.month(.year, month))

---
+++ calendar9_2018.py
@@ -0,0 +1,22 @@
+# Calendar for September 2018
+
+print("This is the calendar for September 2018")
+print("")
+
+days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+week1 = [' 26', ' 27', ' 28', ' 29', ' 30', ' 31', '  1']
+week2 = ['  2', '  3', '  4', '  5', '  6', '  7', '  8']
+week3 = ['  9', ' 10', ' 11', ' 12', ' 13', ' 14', ' 15']
+week4 = [' 16', ' 17', ' 18', ' 19', ' 20', ' 21', ' 22']
+week5 = [' 23', ' 24', ' 25', ' 26', ' 27', ' 28', ' 29']
+week6 = [' 30', '  1', '  2', '  3', '  4', '  5', '  6']
+
+print(" ".join(days))
+print(" ".join(week1))
+print(" ".join(week2)) 
+print(" ".join(week3))
+print(" ".join(week4))
+print(" ".join(week5))
+print(" ".join(week6))
+
+
